0d06cda3de9594544abd90cce642c85f8b7c0ed3
YG <0root.dev@gmail.com>
2019-10-25 15:43:03 +0900

    Add dummy toggle buttons

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,13 +1,34 @@
 <!-- Copyright (C) 2019 Team SaveTheLogin <https://savethelogin.world/> -->
 <template>
-  <div class="col">
+  <div class="col-12 mb-3">
     <h4 class="mt-3">Basics</h4>
     <hr class="mt-0" />
+    <div class="row">
+      <div class="col">
+        <ToggleSwitch />
+      </div>
+    </div>
     <h4 class="mt-3">Experimental</h4>
     <hr class="mt-0" />
+    <div class="row">
+      <div class="col">
+        <ToggleSwitch />
+      </div>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import ToggleSwitch from './ToggleSwitch';
 
-<style></style>
+export default {
+  name: 'Options',
+  components: {
+    ToggleSwitch,
+  },
+};
+</script>
+
+<style>
+/**/
+</style>
