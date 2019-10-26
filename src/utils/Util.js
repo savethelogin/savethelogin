6f60f2a16780ca26c1c4646882aa5e1b8e98680d
YG <0root.dev@gmail.com>
2019-10-27 04:15:21 +0900

    Add executeScript function

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -1,5 +1,18 @@
 /* Copyright (C) 2019 Team SaveTheLogin <https://savethelogin.world/> */
 
+export function executeScript({ tabId, details }) {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.executeScript(
+      tabId,
+      details,
+      results => {
+        resolve(results);
+      },
+      logError
+    );
+  });
+}
+
 export function logError(e) {
   if (chrome.runtime.lastError) {
     console.log(e, chrome.runtime.lastError);
