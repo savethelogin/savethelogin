568a65f6b64e157b19df2e6e1a6fb02317ac3039
YG <0root.dev@gmail.com>
2019-11-04 04:22:43 +0900

    Add i18n plugin

--- src/components/Globals.js
+++ src/components/Globals.js
@@ -4,6 +4,10 @@ import Vue from 'vue';
 import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
 
+import VueChromeI18n from '../plugins/vue-chrome-i18n/src/index';
+
+Vue.use(VueChromeI18n);
+
 const requireComponent = require.context('.', false, /Base[A-Z]\w+\.(vue|js)$/);
 
 requireComponent.keys().forEach(fileName => {
