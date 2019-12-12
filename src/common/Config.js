0ba9f1fa214ff96e24f53fce3624ce30a56ae2f4
YG <0root.dev@gmail.com>
2019-12-13 04:26:22 +0900

    Add api url

--- src/common/Config.js
+++ src/common/Config.js
@@ -9,6 +9,10 @@ class Config {
     this.PROJECT_PREFIX = 'stl';
     this.ID_PREFIX = `__${this.PROJECT_PREFIX}__`;
     this.PROJECT_DOMAIN = 'savethelogin.world';
+    this.API_URL =
+      process.env.NODE_ENV === 'develop'
+        ? 'http://127.0.0.1/'
+        : `https://api.${this.PROJECT_DOMAIN}/`;
   }
 }
 
