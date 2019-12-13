63c576ddc737fcf90399ace029cc0686a134ae49
YG <0root.dev@gmail.com>
2019-12-14 05:02:04 +0900

    Add scheme

--- src/common/Config.js
+++ src/common/Config.js
@@ -10,9 +10,8 @@ class Config {
     this.ID_PREFIX = `__${this.PROJECT_PREFIX}__`;
     this.PROJECT_DOMAIN = 'savethelogin.world';
     this.API_URL =
-      process.env.NODE_ENV === 'develop'
-        ? 'http://127.0.0.1/'
-        : `https://api.${this.PROJECT_DOMAIN}/`;
+      process.env.NODE_ENV === 'development' ? '127.0.0.1' : `api.${this.PROJECT_DOMAIN}`;
+    this.API_SCHEME = process.env.NODE_ENV === 'development' ? 'http' : 'https';
   }
 }
 
