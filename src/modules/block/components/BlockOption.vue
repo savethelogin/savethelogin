1d5cbaa9595f7baa458b032e309b97de5749b7fd
YG <0root.dev@gmail.com>
2019-12-23 10:57:36 +0900

    Fix i18n messages

--- src/modules/block/components/BlockOption.vue
+++ src/modules/block/components/BlockOption.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h4 class="mb-2 mt-3">Block</h4>
+    <h4 class="mb-2 mt-3" v-chrome-i18n>__MSG_block__</h4>
     <hr class="mt-0" />
-    <BaseButton v-bind:callback="openWhitelist" class="btn-sm">Whitelist</BaseButton>
+    <BaseButton v-bind:callback="openWhitelist" class="btn-sm" v-chrome-i18n
+      >__MSG_whitelist__</BaseButton
+    >
   </div>
 </template>
 
