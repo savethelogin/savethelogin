9b91a40931c2b86022a3b8b02c3285a8499d5e1e
YG <0root.dev@gmail.com>
2019-12-23 10:42:05 +0900

    Fix #27

---
+++ src/modules/block/components/BlockOption.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h4 class="mb-2 mt-3">Block</h4>
+    <hr class="mt-0" />
+    <BaseButton v-bind:callback="openWhitelist" class="btn-sm">Whitelist</BaseButton>
+  </div>
+</template>
+
+<script>
+import { createTab } from '@/common/Utils';
+
+export default {
+  methods: {
+    openWhitelist: function() {
+      createTab({ url: `/block-whitelist.html` });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/**/
+</style>
